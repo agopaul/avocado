7e148ee8f86900765d5911dbf1cae98645707845
paul.ago@gmail.com <paul.ago@gmail.com>
2010-12-22 19:43:39 +0100

    * Added class AvocadoSchemaDiff with methods firstHasTable, secondtHasTable

--- lib/Avocado.php
+++ lib/Avocado.php
@@ -96,7 +96,7 @@ class AvocadoSchema{ // aka AvocadoTables
 	public function toJson(){
 		return json_encode($this->toArray());
 	}
-
+	
 }
 
 /**
@@ -250,5 +250,46 @@ class AvocadoField{
 	}
 	
 }
+
+/**
+ * Handles two schemas' differences
+ *
+ * @package default
+ * @author paul
+ **/
+class AvocadoSchemaDiff{
+	
+	protected $FirstHas, $SecondtHas;
+	
+	/**
+	 * undocumented function
+	 *
+	 * @return void
+	 * @author paul
+	 **/
+	function __construct(){}
+	
+	/**
+	 * Insert a new Table which secondSchema doesn't have
+	 *
+	 * @return void
+	 * @author paul
+	 **/
+	function firstHasTable(AvocadoTable $Table){
+		$this->FirstHas[] = $Table;
+	}
+	
+	/**
+	 * Insert a new Table which firstSchema doesn't have
+	 *
+	 * @return void
+	 * @author paul
+	 **/
+	function secondtHasTable(AvocadoTable $Table){
+		$this->SecondtHas[] = $Table;
+	}
+	
+}
+
 
 ?>
