2199281a5e4e2e48fddb2ea3df539173fbdd24aa
paul.ago@gmail.com <paul.ago@gmail.com>
2011-01-12 23:44:03 +0100

    * Added AvocadoSchema::fromArray(), pupulates the object via array * Added singleton methods AvocadoSchema::getInstanceFromDb(), AvocadoSchema::getInstanceFromArray()

--- lib/Avocado.php
+++ lib/Avocado.php
@@ -55,24 +55,35 @@ class AvocadoSchema{ // aka AvocadoTables
 	protected $Db;
 	protected $Tables;
 	
-	function __construct(PDO $Db){
-		$this->Db = $Db;
-		$this->fromDb();
+	function __construct(PDO $Db=null, array $Arr=null){
+		if($Db && !$Arr)
+			$this->fromDb($Db);
+		elseif(!$Db && $Arr)
+			$this->fromArray($Arr);
+		else throw new AvocadoException("You must provide a PDO instance or an array");
 	}
 	
-	public static function getInstance(PDO $Db){
+	public static function getInstance(PDO $Db=null, array $Arr=null){
+		return new self($Db, $Arr);
+	}
+
+	public static function getInstanceFromDb(PDO $Db){
 		return new self($Db);
+	}
+
+	public static function getInstanceFromArray(array $Arr){
+		return new self(null, $Arr);
 	}
 	
 	public function getTables(){
 		return $this->Tables;
 	}
 	
-	protected function fromDb(){
-		foreach($this->Db->query("SHOW TABLES") as $Table){
+	protected function fromDb($Db){
+		foreach($Db->query("SHOW TABLES") as $Table){
 			$Tablename = $Table[0];
 			$Fields = array();
-			foreach($this->Db->query("SHOW COLUMNS FROM $Tablename") as $Field){
+			foreach($Db->query("SHOW COLUMNS FROM $Tablename") as $Field){
 				preg_match("/(\w+)((\([\d]+)\))?/", $Field['Type'], $Matches);
 				$Fields[] = new AvocadoField($Field['Field'],
 											$Matches[1],
@@ -81,6 +92,21 @@ class AvocadoSchema{ // aka AvocadoTables
 			}		
 			
 			$this->Tables[] = new AvocadoTable($Tablename, $Fields);
+		}
+		return true;
+	}
+
+	protected function fromArray($Input){
+		foreach($Input as $TableName=>$Table){
+			$Fields = array();
+			foreach($Table as $Field){
+				$Fields[] = new AvocadoField($Field['name'],
+											$Field['type'],
+											(bool)$Field['nullable'],
+											(int)$Field['length']);
+			}		
+			
+			$this->Tables[] = new AvocadoTable($TableName, $Fields);
 		}
 		return true;
 	}
