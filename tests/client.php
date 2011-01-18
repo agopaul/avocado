5bdbf7546b63b9f96fbca7434555abb466175e8d
paul.ago@gmail.com <paul.ago@gmail.com>
2011-01-18 22:19:33 +0100

    * Added .gitignore for client.php and server.php * Deleted client.php

--- tests/client.php
+++ tests/client.php
@@ -3,6 +3,8 @@
 require __DIR__."/../lib/Avocado.php";
 
 $Db = new PDO('mysql:host=localhost;dbname=films2', "root", "qwerty");
-AvocadoRestClient::getInstance($Db, array())->compareTo("http://hydra/Avocado/tests/server.php");
+AvocadoRestClient::getInstance($Db, array())->compareTo("http://hydra/avocado/tests/server.php");
+
+
 
 ?>
